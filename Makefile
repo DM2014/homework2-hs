a0522d4360f127d14cf6ba7f9fcc4a714bcd20ac
banacorn <banacorn@gmail.com>
2014-04-26 23:21:21 +0800

    profiling heap

--- Makefile
+++ Makefile
@@ -4,3 +4,19 @@ a:
 c:
 	cabal build
 	cat data/c | time ./dist/build/homework2-hs/homework2-hs 0.5 0.5 10 +RTS -K2000m -H500m -RTS
+
+
+
+raw:
+	cabal build
+	cat /media/banacorn/8258dc47-a771-439e-9a7c-04c04ddf9ccf/d/all.txt |\
+	 time ./dist/build/homework2-hs/homework2-hs 0.5 0.5 10 +RTS -K2000m -H500m -RTS > output+
+
+part:
+	cabal build
+	cat /media/banacorn/8258dc47-a771-439e-9a7c-04c04ddf9ccf/d/all.txt | head -n1100k |\
+	 time ./dist/build/homework2-hs/homework2-hs 0.5 0.5 10 +RTS -K2000m -H500m  -sstderr -p -hy -RTS > output+
+
+
+	hp2ps -c homework2-hs.hp
+	gv homework2-hs.ps 
---
+++ src/Parser.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+
+module Parser (processRawData) where
+
+import              Control.Monad (replicateM_, void)
+import              Control.Monad.Trans.Resource
+import              Data.Attoparsec.ByteString.Lazy
+import              Data.ByteString (ByteString)
+import              Data.Conduit
+import              Data.Conduit.Attoparsec
+import qualified    Data.Conduit.Binary as CB
+import              Data.Monoid ((<>))
+import              Prelude hiding (take)
+import              System.IO (stdin, stdout)
+
+
+import qualified    Data.HashMap.Strict as H
+import              Data.Set (Set)
+import qualified    Data.Set as Set
+import qualified    Data.ByteString as B
+import qualified    Data.ByteString.Char8 as B8
+
+data Product = Product UserID ProductID
+type ProductID = ByteString
+type UserID = ByteString
+type Table a = H.HashMap UserID a
+type ItemSet = Set ProductID
+
+
+printProduct :: Product -> ByteString
+printProduct (Product u p) = u <> " " <> p <> "\n"
+
+processRawData :: ResourceT IO ()
+processRawData = CB.sourceHandle stdin $$ rawDataParserConduit =$= filterUnknown =$= accumulateProduct H.empty =$= toByteString =$ CB.sinkHandle stdout
+
+
+toByteString :: Conduit (Table ItemSet) (ResourceT IO) ByteString
+toByteString = do
+    t <- await
+    case t of
+        Just table -> yield $ B8.pack $ show $ H.size  table-- mapM_ (yield . toLine) (H.toList table)
+        Nothing -> return ()
+    --where   toLine (_, v) = Set.foldl' (\a b -> a <> " " <> b) B.empty v <> "\n"
+
+
+filterUnknown :: Conduit Product (ResourceT IO) Product
+filterUnknown = do
+    result <- await
+    case result of
+        Just (Product "unknown" _        ) -> filterUnknown
+        Just (Product _         "unknown") -> filterUnknown
+        Just (Product u         p        ) -> yield (Product u p) >> filterUnknown
+        Nothing -> return ()
+
+rawDataParserConduit :: Conduit ByteString (ResourceT IO) Product
+rawDataParserConduit = do
+    conduitParserEither parserSection =$= awaitForever go
+    where   go (Left s) = error $ show s
+            go (Right (_, p)) = yield p
+
+accumulateProduct :: Table ItemSet -> Conduit Product (ResourceT IO) (Table ItemSet)
+accumulateProduct table = do
+    p <- await
+    case p of
+        Just (Product userID productID) -> accumulateProduct $ H.insertWith (\new set -> new `Set.union` set) userID (Set.singleton productID) table
+        Nothing -> yield table
+
+
+
+
+
+dropLine :: Parser ()
+dropLine = skipWhile (/= 0xa) >> void (take 1) 
+
+parseLine :: Parser ByteString
+parseLine = do
+    a <- takeTill (== 0xa)
+    take 1
+    return a
+
+-- | raw data
+
+parseProduct :: Parser ProductID
+parseProduct = string "product/productId: " >> parseLine
+
+parseUser :: Parser UserID
+parseUser = string "review/userId: " >> parseLine
+
+parserSection :: Parser Product
+parserSection = do
+    productID <- parseProduct
+    replicateM_ 2 dropLine
+    userID <- parseUser
+    replicateM_ 6 dropLine
+    choice [dropLine, endOfInput]
+    return (Product userID productID)
